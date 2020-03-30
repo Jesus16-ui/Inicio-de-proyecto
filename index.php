db3612c911c48d54cfc23612d1c7c6f61f440c9b
Jesus16-ui <gonzalez.jesus@gaussjordan.edu.mx>
2020-03-30 14:01:29 -0600

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="index.css">
+    <title>Pagina web</title>
+</head>
+<body style= background=Pagina/Degradado.jpg>
+
+
+    <nav id="Nav">
+    <h1>Álbum Colores</h1>
+    </nav>
+
+&nbsp 
+
+
+    <div class="Contenido">
+        <h1> Amarillo </h1>
+        <p>Es la primera cancion del álbumproducida por dj snake, co-producida por afro bros, el mismo cantante 
+            menciona que la cancion y su inspiracion fue para que la gente empezara a escuchar el album con una 
+            buena vibra 
+
+</p>
+
+
+
+</div>
+<div align="center">
+<img src="Amarillo.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+
+&nbsp
+
+<div class="Contenido1">
+
+        <h1> Azul </h1>
+        <p> Es una cancion producida por Sky estuvo en colaboracion indirecta por J Quiles en el lugar de Miami, es un tema para vivir la vida sin tener que preocuparse  
+</p>
+
+&nbsp
+
+</div>
+<div align="center">
+<img src="Azul.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Con">
+
+        <h1> Rojo </h1>
+        <p> Es una canción que tiene un video el cual muestra mas emociones de las que tienes pensadas, es una cancion que te pone a reflexionar sobre la vida
+        y los seres queridos que pueden llegar a ya no estar 
+
+        </p>
+
+        &nbsp
+
+</div>
+<div align="center">
+<img src="Rojo.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Contenido3">
+
+        <h1> Rosa </h1>
+        <p> Fue una cancion producida por Diplo, y el video esta proximo a salir  
+
+        </p>
+
+        &nbsp
+
+</div>
+<div align="center">
+<img src="Rosa.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Contenido4">
+
+        <h1> Negro </h1>
+        <p> Es una cancion que es pensada y escrita sobre la calle, es una cancion que hace tiempo ya no se escuchan
+        esta cancion fue inspirada gracias a artistas de la escena anterior
+
+        </p>
+
+        &nbsp
+
+</div>
+<div align="center">
+<img src="Negro.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Contenido5">
+
+        <h1> Morado </h1>
+        <p> Es la segunda cancion presentada del álbum  
+
+        </p>
+
+        &nbsp
+
+</div>
+<div align="center">
+<img src="Morado.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Contenido6">
+
+        <h1> Blanco </h1>
+        <p> Es la primera cancion que muestra el álbum, con un distinto estilo y sonido a las demas canciones de 
+        todo el álbum
+
+
+        </p>
+
+        &nbsp
+
+</div>
+<div align="center">
+<img src="Blanco.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Contenido7">
+
+        <h1> Verde </h1>
+        <p> Es una de las unicas canciones del álbum que tienen una colaboración y es el lanzamiento como cantante del
+        productor Sky 
+
+        </p>
+
+        &nbsp
+
+</div>
+<div align="center">
+<img src="Verde.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Contenido8">
+
+        <h1> Gris </h1>
+        <p> Es un tema producido por Sky y Michael Brown el cual es de Haití, es un tema el cual representa la
+        desconfianza que te demuestra una persona   
+
+        </p>
+
+        &nbsp
+
+</div>
+<div align="center">
+<img src="Gris.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+&nbsp
+
+<div class="Contenido9">
+
+        <h1> Arcoiris </h1>
+        <p> Es el segundo tema que tiene una colaboración con el cantante MR.Eazi el cual
+         ya habia trabajado antes con el artista, el tema es llamado arcoiris por los distintas personas
+         que trabajan en el tema ya que son de distintos paises y demuestra los distintos ritmos que tiene la canción 
+
+
+        </p>
+
+        &nbsp
+    
+</div>
+<div align="center">
+<img src="Arcoiris.jpg" alt="" 
+width="400"
+height="400">
+</div>
+
+</body>
+
+</html>  
